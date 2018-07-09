6d39849b29bf4c771a2017099db0217b7e4377f0
Goular <zhaojt_cs@126.com>
2018-07-09 10:12:38 +0800

    提交HTTPS的内容

--- main.go
+++ main.go
@@ -54,6 +54,17 @@ func main() {
 		log.Info("The router has been deployed successfully.")
 	}()
 
+	// 开启HTTPS服务器功能
+	cert := viper.GetString("tls.cert")
+	key := viper.GetString("tls.key")
+	if cert != "" && key != "" {
+		go func() {
+			log.Infof("Start to listening the incoming requests on https address: %s", viper.GetString("tls.addr"))
+			log.Info(http.ListenAndServeTLS(viper.GetString("tls.addr"), cert, key, g).Error())
+		}()
+	}
+
+	// 开启普通HTTP服务器的功能
 	log.Infof("Start to listening the incoming requests on http address: %s", viper.GetString("addr"))
 	log.Info(http.ListenAndServe(viper.GetString("addr"), g).Error())
 }
