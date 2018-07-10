b496626d5e1d8efdaeb5e1d220029f2ed578c356
Goular <zhaojt_cs@126.com>
2018-07-10 16:38:47 +0800

    添加文档提交

--- handler/user/create.go
+++ handler/user/create.go
@@ -10,7 +10,14 @@ import (
 	"spoon/model"
 )
 
-// Create creates a new user account.
+// @Summary Add new user to the database
+// @Description Add a new user
+// @Tags user
+// @Accept  json
+// @Produce  json
+// @Param user body user.CreateRequest true "Create a new user"
+// @Success 200 {object} user.CreateResponse "{"code":0,"message":"OK","data":{"username":"kong"}}"
+// @Router /user [post]
 func Create(c *gin.Context) {
 	log.Info("User Create function called.", lager.Data{"X-Request-Id": util.GetReqID(c)})
 	var r CreateRequest
