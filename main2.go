c47ffc1b005231b94e31e16dabdcd959f9ff0ea8
Goular <zhaojt_cs@126.com>
2018-07-17 17:12:34 +0800

    添加测试服务器的内容

--- main2.go
+++ main2.go
@@ -19,9 +19,9 @@ func hello(c *gin.Context) {
 
 	//配置微信参数
 	config := &wechat.Config{
-		AppID:          "wxff730b6a013a40ca",
-		AppSecret:      "6b555e0409840238686e6d5d8f76fdb5",
-		Token:          "mengmeizi",
+		AppID:          "wx5991d31418d74a4d",
+		AppSecret:      "9f3a0acb95255d428846898e258e120c",
+		Token:          "dazhen",
 		EncodingAESKey: "AxKX1vmaaVgi1GyNxOeFn5OFoYN0l16wmmzE1GKHh5c",
 	}
 	wc := wechat.NewWechat(config)
