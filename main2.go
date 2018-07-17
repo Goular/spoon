30a7628f02f16c5d8d522f1b49258e6bae403bba
Goular <zhaojt_cs@126.com>
2018-07-17 16:39:16 +0800

    添加main2.go的内容

--- main2.go
+++ main2.go
@@ -1,7 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+	"github.com/silenceper/wechat"
+	"github.com/silenceper/wechat/message"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	router := gin.Default()
+
+	router.Any("/", hello)
+	router.Run(":8001")
+}
+
+func hello(c *gin.Context) {
+
+	//配置微信参数
+	config := &wechat.Config{
+		AppID:          "wxff730b6a013a40ca",
+		AppSecret:      "6b555e0409840238686e6d5d8f76fdb5",
+		Token:          "mengmeizi",
+		EncodingAESKey: "AxKX1vmaaVgi1GyNxOeFn5OFoYN0l16wmmzE1GKHh5c",
+	}
+	wc := wechat.NewWechat(config)
+
+	// 传入request和responseWriter
+	server := wc.GetServer(c.Request, c.Writer)
+	//设置接收消息的处理方法
+	server.SetMessageHandler(func(msg message.MixMessage) *message.Reply {
+
+		//回复消息：演示回复用户发送的消息
+		text := message.NewText(msg.Content)
+		return &message.Reply{MsgType: message.MsgTypeText, MsgData: text}
+	})
+
+	//处理消息接收以及回复
+	err := server.Serve()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	//发送回复的消息
+	server.Send()
 }
