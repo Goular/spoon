007a88f830839480f7783b670cfed1e8d8294368
Goular <zhaojt_cs@126.com>
2018-07-18 15:13:47 +0800

    提交内容

--- wechatTest.go
+++ wechatTest.go
@@ -4,12 +4,11 @@ import (
 	"github.com/gin-gonic/gin"
 	_ "spoon/handler/wechat"
 	"spoon/handler/wechat"
-	"github.com/spf13/viper"
 )
 
 // 微信公众号测试
 func main() {
 	router := gin.Default()
 	router.Any("/wechat/reply", wechat.Reply)
-	router.Run(viper.GetString("addr"))
+	router.Run(":8001")
 }
