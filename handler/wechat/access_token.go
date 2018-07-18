a998be41acf43c93f882700e441094f78cda973d
Goular <zhaojt_cs@126.com>
2018-07-18 15:56:24 +0800

    提交内容

--- handler/wechat/access_token.go
+++ handler/wechat/access_token.go
@@ -3,18 +3,14 @@ package wechat
 import (
 	"github.com/gin-gonic/gin"
 	"net/http"
-	"fmt"
 )
 
 // 返回access_token
 func AccessToken(c *gin.Context) {
 	access_token, err := Wechat.GetAccessToken()
-	fmt.Println("112233")
-	fmt.Println(access_token)
-	fmt.Println("9966")
 	if err != nil {
-		c.JSON(http.StatusOK, access_token)
-	} else {
 		c.JSON(http.StatusOK, nil)
+	} else {
+		c.JSON(http.StatusOK, access_token)
 	}
 }
